08bc74db19f85e433c46f2acdc3a56c52cfd317e
boybenson <ybenson96@gmail.com>
2024-07-01 19:15:31 +0000

    feat: changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,5 @@ export const server = `http://localhost:8080`;
 
 export const createUserApi = `${server}/api/user/create-user`;
 export const getUsersApi = `${server}/api/user/get-users`;
+
+export const loginApi = `${server}/api/user/auth/signin`;
