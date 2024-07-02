298511a1410dab4c0426c5d77c9a2cfbab7ce8f4
ericatsu <ekaatsu@st.ug.edu.gh>
2024-07-02 14:18:02 +0000

    ✅

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { loginApi } from "../../constants";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import logo from '../../assets/logo.png';
+import doctors from '../../assets/doctors.png';
 
 type formInput = {
   email: string;
@@ -29,108 +30,97 @@ export default function SignIn({ setShowModal }: any) {
   };
 
   return (
-    <>
-      <div className="flex h-screen flex-1">
-        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
+    <div className="bg-white flex h-screen">
+      <div >
+        <div>
+          <div className=" flex-1 absolute w-[700px] h-screen top-0 left-0 bg-[#869ca3] overflow-hidden">
+            <div className="inline-flex items-center absolute top-[50px] left-32">
               <img
-                className="h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
+                className="relative w-[50px] h-[50px]"
+                alt="Logo"
+                src={logo}
               />
-              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
-              </h2>
+              <div className="relative w-fit mt-[-2px] [font-family:'Inter-Medium',Helvetica] font-medium text-white text-[50px] text-center tracking-[0] leading-[normal]">
+                UG Hospital
+              </div>
             </div>
-
-            <div className="mt-10">
-              <div>
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Email address
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        {...register("email", {
-                          required: true,
-                        })}
-                        type="email"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Password
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        {...register("password", {
-                          required: true,
-                        })}
-                        type="password"
-                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      />
-                      <label
-                        htmlFor="remember-me"
-                        className="ml-3 block text-sm leading-6 text-gray-700"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-
-                    <div className="text-sm leading-6">
-                      <a
-                        href="#"
-                        className="font-semibold text-indigo-600 hover:text-indigo-500"
-                      >
-                        Forgot password?
-                      </a>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      {loading ? "Loading....." : "Sign In"}
-                    </button>
-                  </div>
-                </form>
+            <div className="absolute w-[1161px] h-1/2 top-40 left-[-225px]">
+              <div className="relative h-[1161px] rounded-[580.5px]">
+                <div className="absolute w-[1013px] h-[951px] top-[105px] left-[68px] bg-[#0f4770] rounded-[506.64px/475.51px]" />
+                <div className="absolute w-[1161px] h-[1161px] top-0 left-0 rounded-[580.5px] border-[29.3px] border-solid border-[#0f4770]" />
               </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block">
+          <div className="inline-flex flex-col items-center w-1/2 justify-end gap-10 absolute top-[150px] left-1/2">
+            <div className="flex flex-col items-center gap-2">
+              <div className="text-5xl font-bold text-black">
+                Welcome back
+              </div>
+              <p className="text-xl text-[#878787]">
+                UG Hospital - Department of Procurement Portal
+              </p>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-start gap-4">
+              <div className="flex flex-col items-start gap-1">
+                <label htmlFor="email" className="text-lg text-black">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  {...register("email", {
+                    required: true,
+                  })}
+                  className="w-[490px] p-2 rounded-[10px] border border-solid border-[#b5b6b7]"
+                  placeholder="Email address"
+                />
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <label htmlFor="password" className="text-lg text-black">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  {...register("password", {
+                    required: true,
+                  })}
+                  className="w-[490px] p-2 rounded-[10px] border border-solid border-[#b5b6b7]"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" className="w-[491px] py-3.5 bg-[#0f4770] rounded-[10px] text-white font-bold">
+                {loading ? "Loading....." : "Sign In"}
+              </button>
+              <div className="flex w-[490px] items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-3 block text-sm leading-6 text-gray-700"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-[#0f4770] font-medium">
+                  Forgot password?
+                </div>
+              </div>
+            </form>
+          </div>
           <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-            alt=""
+            className="absolute w-[800px] h-[58.7vh] top-[280px] left-0 object-cover"
+            alt="Doctors"
+            src={doctors}
           />
         </div>
       </div>
-    </>
+    </div>
   );
 }
